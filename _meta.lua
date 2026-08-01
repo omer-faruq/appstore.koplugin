40f8737817555fb13cdf6a21f03c304771ec72c8
omerfarukinel <85738646+omer-faruq@users.noreply.github.com>
2026-08-01 15:53:48 +0300

    v1.11.0

--- _meta.lua
+++ _meta.lua
@@ -4,6 +4,6 @@ return {
     name = "appstore",
     fullname = _("App Store"),
     description = _([[Discover and manage community plugins and patches from GitHub topics.]]),
-    version = "1.10.0",
+    version = "1.11.0",
 }
 
